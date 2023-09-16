ea3279507f288975c6d3841d66e7b9fde042b248
TreyLiu <jayqunding1988@163.com>
2023-09-16 11:28:11 +0800

    commit src file

---
+++ pages/Market_data_analysis.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+
+def page_2():
+    st.set_page_config(page_title="市场数据分析", page_icon=":chart_with_upwards_trend:")
+    st.write("市场数据分析")
+
+
+
+
+if __name__ == "__main__":
+    page_2()
